eeb68efca42a297ec9539a6a4e4ec5df47295141
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-08 13:52:49 -0400

    ternary and && operators

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -2,20 +2,23 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const HelloWorld = () => {
+  let isHello = false;
+  // return <span>{isHello ? "Hello" : "Goodbye"}</span>;
   return (
-    <div>
-      <Hello />
-    </div>
-  );
+  <span>
+    {isHello && 'Hello'}
+    {!isHello && 'Goodbye'}
+  </span>
+  )
 };
 
-const Hello = () => {
-  let name = "World";
-  return <span>Hello {name}</span>;
-};
+// const Hello = () => {
+//   let name = "World";
+//   return <span>Hello {name}</span>;
+// };
 
-const World = () => {
-  return <span>World</span>;
-};
+// const World = () => {
+//   return <span>World</span>;
+// };
 
 ReactDom.render(<HelloWorld />, document.querySelector("#root"));
