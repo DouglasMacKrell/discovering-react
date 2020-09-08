f76b57f7a128364c01094964c3c127366f61d043
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-08 15:53:26 -0400

    props groundwork

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const HelloWorld = () => {
-  let isHello = false;
-  // return <span>{isHello ? "Hello" : "Goodbye"}</span>;
-  return (
-  <span>
-    {isHello && 'Hello'}
-    {!isHello && 'Goodbye'}
-  </span>
-  )
-};
+// const HelloWorld = () => {
+//   let isHello = false;
+//   // return <span>{isHello ? "Hello" : "Goodbye"}</span>;
+//   return (
+//   <span>
+//     {isHello && 'Hello'}
+//     {!isHello && 'Goodbye'}
+//   </span>
+//   )
+// };
 
 // const Hello = () => {
 //   let name = "World";
@@ -21,4 +21,36 @@ const HelloWorld = () => {
 //   return <span>World</span>;
 // };
 
-ReactDom.render(<HelloWorld />, document.querySelector("#root"));
+// function Doug() {
+//   const fName = "Doug"
+//   const lName = "MacKrell"
+
+//   return (
+//     <Person age="105" name={fName + " " + lName} />
+//   )
+// }
+
+// const Person = props => <h1>{props.name + " " + props.age}</h1>
+
+// function Hello (props) {
+//   return (
+//     <span>
+//       Hello {props.name}
+//     </span>
+//   )
+// }
+
+// const Hello = ({ name }) => <span>Hello, {name}</span>;
+
+// const Hello = ({ fName, lName }) => <span>Hello, {fName} {lName}</span>;
+
+const Hello = (props) => {
+  const { name } = props
+  return (
+  <span>
+    Hello, { name }
+  </span>
+  )
+  };
+
+ReactDom.render(<Hello name="Doug" />, document.querySelector("#root"));
